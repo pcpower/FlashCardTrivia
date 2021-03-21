c3da0cf0bfc5df39ed74ca037c3f815a0917ba3c
Morgan Corbridge <pcpower@uw.edu>
2021-03-21 16:46:50 -0700

    Added Timer - Works

--- trivia.js
+++ trivia.js
@@ -1,6 +1,4 @@
-
-
-const startButton = document.getElementById('start-btn')//
+const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 
@@ -12,27 +10,61 @@ const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
 
-class triviaWrapper {
+class trivia {
     //Private Properties
     #BASE_URL = 'https://opentdb.com/api.php?amount=50';
 
     // Public Properties
+    currentQuestionIndex = 0;
     amount = 10;
     category = "";
     difficulty = "";
     type = "multiple";
+    score = 0;
+    questionCount = 0;
 
     constructor(type) {
         this.type = type;
-        
+        this.getCategoryList();
+        // this.setNextQuestion();
+        startButton.addEventListener('click', () => {
+            this.resetScore();
+                
+            let time = 600,
+            display = document.querySelector('#countdown');
+            this.startTimer(time, display, function() {alert('Times up!'); });
+            
+            
+            this.startGame(); 
+            this.clearStatusClass(document.body)
+            this.resetState(); 
+            // if (this.questionCount < questions.length) {
+               this.showQuestion(shuffledQuestions[this.currentQuestionIndex])
+                
+            // }
+            console.log(questions.length);
+           
+        })
+
+        nextButton.addEventListener('click', () => {
+            this.currentQuestionIndex++
+            this.resetState(); 
+            this.showQuestion(shuffledQuestions[this.currentQuestionIndex])
+            this.clearStatusClass(document.body);
+        })
     }
 
+    clearStatusClass(element) {
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
+        console.log("7")
+    }
 
-    getCategoryList() {
+    getQuestions() {
     //     this.difficulty = e.target.options[e.target.options.selectedIndex].value;
     //     console.log(this.difficulty);
 
-        let categoryList = new Array();
+        // let categoryList = new Array();
 
 
         fetch(this.#BASE_URL)
@@ -44,139 +76,204 @@ class triviaWrapper {
 
             console.log(jsonPackage.results);
             
-            const questionList = jsonPackage.results;
+            // const questionList = jsonPackage.results;
             
         
-            questionList.forEach(function(question) {
-                console.log(question.category);
-                if (!categoryList.includes(question.category)) {
-                    categoryList.push(question.category);
+            // // questionList.forEach(function(question) {
+            // //     console.log(question.category);
+            // //     if (!categoryList.includes(question.category)) {
+            // //         categoryList.push(question.category)
                     
-                }
+            // //     }
 
-            });
-
+            // });
+        });
+    };
+            
+    getCategoryList() {
             categoryList.sort();
             console.log(categoryList);
-          
-        });        
 
-            // Adding Categories to the dropdown list
-        const CategoryListSelect = document.getElementById("categoryAdd");
-        let opt1 = document.createElement("option");
+              // Adding Categories to the dropdown list
+            const CategoryDropDown = document.getElementById("categoryAdd");
 
-        opt1.value = "1";
-        opt1.text = "option: Value 1"
-
-        CategoryListSelect.add(opt1, null);
+        categoryList.forEach(function(category) {
             
+        let opt1 = document.createElement("option");
+        console.log(category);
+        console.log("1");
+
+        opt1.value = category.id;
+        opt1.text = category.name;
+
+        CategoryDropDown.add(opt1, null);
+
+        
+        })   
+
+        console.log("4");
+
+          
         return "done";
+          
+         
+
+        
     
-    }
+    };
+
+    
 
     getTriviaQuestion() {
         
         return;
-    }
+    };
 
     getTypeList() {return;}
 
+    resetScore() {
+        this.score = 0;
+    };
+
+    resetState() {
+        // otrivia.clearStatusClass(document.body)
+        nextButton.classList.add('hide')
+        while (answerButtonsElement.firstChild) {
+            answerButtonsElement.removeChild
+            (answerButtonsElement.firstChild)
+            
+        }
+        console.log("4")
+    }
+
+    selectAnswer(e) {
+        const selectedButton = e.target
+        const correct = selectedButton.dataset.correct
+
+        console.log(correct);
+
+        Array.from(answerButtonsElement.children).forEach(button => {
+            otrivia.setStatusClass(button, button.dataset.correct)
+            button.removeEventListener('click', otrivia.selectAnswer);
+        })
+
+        if (correct) {
+            document.body.classList.add('correct')
+            otrivia.score ++;
+        } else {        
+            document.body.classList.add('wrong')
+        }
+
+        console.log(`the Score is ${otrivia.score}`);
+
+        if (shuffledQuestions.length > otrivia.currentQuestionIndex + 1) {
+            nextButton.classList.remove('hide')
+        } else {
+            startButton.innerText = 'Restart'
+            startButton.classList.remove('hide')
+        }
+        console.log("5")
     
+    };
+
+    // setNextQuestion() {
+    //     resetState() {
+    //     showQuestion(shuffledQuestions[this.currentQuestionIndex])
+    //     console.log("2")
+    
+    // };
+
+    setStatusClass(element, correct) {
+        // otrivia.clearStatusClass(element)
+        console.log(element);
+        if (correct) {
+            element.classList.add('correct')
+        } else {
+            element.classList.add('wrong')
+        }
+        
+
+    }
+
+    showQuestion(question) {
+        questionElement.innerText = question.question;
+        question.answers.forEach(answer => {
+            const button = document.createElement('button');
+            button.innerText = answer.text;
+            button.classList.add('btn');
+            if (answer.correct) {
+                button.dataset.correct = answer.correct;
+            }
+            button.addEventListener('click', this.selectAnswer);
+            answerButtonsElement.appendChild(button);
+        })
+        questionCount++;
+    };
+
+    startGame() {
+        console.log('Started');
+        startButton.classList.add('hide');
+        shuffledQuestions = questions.sort(() => Math.random() - .5);
+        this.currentQuestionIndex = 0;
+        questionContainerElement.classList.remove('hide');
+        // categoryListElement.classList.remove('hide')
+        // this.setNextQuestion();
+        console.log("1");
+    };
+
+    startTimer(duration, display, callback) {
+    
+        let timer = duration,
+        minutes, seconds;
+    
+        let myInterval = setInterval(function() {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent = minutes + ":" + seconds;
+    
+            if (--timer < 0) {
+                clearInterval(myInterval);
+    
+                if(callback) {
+                    callback();
+                }
+            }
+        }, 1000);
+    };
 
     
-}
+
+};
+
+const categoryList = [
+    
+    {id: 20, name: 'Mythology'},
+    {id: 27, name: 'Animals'},
+    {id: 21, name: 'Sports'}
+
+];
 
 const multipleType = "multiple";
-const oTriviaWrapper = new triviaWrapper(multipleType)
+const otrivia = new trivia(multipleType)
 
-// difficultySet.addEventListener('change', function(e) {oTriviaWrapper.getCategoryList(e)});
+// difficultySet.addEventListener('change', function(e) {otrivia.getCategoryList(e)});
 
-oTriviaWrapper.getCategoryList();
+// otrivia.getCategoryList();
 
 
-startButton.addEventListener('click', startGame)//
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
 
-function startGame() {//
-    console.log('Started')//
-    startButton.classList.add('hide')//
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    // categoryListElement.classList.remove('hide')
-    setNextQuestion()
-    console.log("1")
-}
 
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-    console.log("2")
+// button.onclick = function() {
+//     window.clearInterval();
+//     let time = 600,
+//     display = document.querySelector('#countdown');
+//     startTimer(time, display, function() {alert('Times up!'); });
 
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-        console.log("3")
-    })
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
-        
-    }
-    console.log("4")
-}
-
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
-  console.log("5")
-
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-    console.log("6")
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-    console.log("7")
-}
-
+// };
 
 const questions = [
     {
@@ -187,5 +284,15 @@ const questions = [
             {text: '7', correct: false},
             {text: '2', correct: false}
         ]
+    },
+    {
+        question: 'What is 2 + 3 = ?',
+        answers: [
+            {text: '5', correct: true },
+            {text: '22', correct: false},
+            {text: '7', correct: false},
+            {text: '2', correct: false}
+        ]
     }
 ];
+
